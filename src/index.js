884e813d8df7b51ae7cc7eb51ef3984c33732f8f
Ariana <ajadrejci@unipu.hr>
2024-03-20 16:15:33 +0100

    add cors

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello world")
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send("Hello world in browser")
+})
+
+app.listen(port, () => console.log(`listening on port ${port}`))
